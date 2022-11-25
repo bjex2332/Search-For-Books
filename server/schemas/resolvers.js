0d98cf17954f3a98c14bbc966ec6bcb80b51488c
unknown <bjex2332@gmail.com>
2022-11-25 13:04:24 -0700

    Resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,30 +6,54 @@ const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne ({
-                    _id: context.user._id }).select('-__v -password')
-                    return userData;
-                }
-                throw new AuthenticationError('You need to be logged in!');
+                const userData = await User.findOne({
+                    _id: context.user._id
+                }).select('-__v -password')
+                return userData;
             }
+            throw new AuthenticationError('You need to be logged in!');
         }
-},
-Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-        const user = await User.create({ username, email, password });
-        const token = signToken(user);
-        return { token, user };
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('User does not exist, please create new account')
+            }
+            correctPassword = await user.isCorrectPassword(password);
+            if (!correctPassword) {
+                throw new AuthenticationError('Incorrect Password')
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updateUser = await User.fineOneAndUpdate (
+                    {_id: context.user._id},{ $pull: { savedBooks: { bookId}}}, {new: true}
+                )
+                return updateUser;
+            }
+            throw new AuthenticationError('Please log in')
+        },
+        saveBook: async (parent, {book}, context) => {
+            if (context.user) {
+                const updateUser = await User.fineOneAndUpdate (
+                    {_id: context.user._id},{ $addToSet: { savedBooks: { book}}}, {new: true}
+                )
+                return updateUser;
+            }
+            throw new AuthenticationError('Please log in')
+        }
+    },
+};
         
-    login: async (parent, {email, password }) => {
-        const user = await User.findOne({email});
-        if (!user) {
-            throw new AuthenticationError('User does not exist, please create new account')
-        }
-        correctPassword = await user.isCorrectPassword(password);
-        if (!correctPassword) {
-            throw new AuthenticationError('Incorrect Password')
-        }
-        const token = signToken(user);
-        return { token, user };
-    }
-}
+
+
+module.exports = resolvers;
